54c4cac663d98ec029f9585bc7f4834f030327fa
Hao <haolim84@gmail.com>
2026-05-31 21:30:37 +0800

    refactor db engine to use config settings

--- src/ask_sg/core/database.py
+++ src/ask_sg/core/database.py
@@ -1,19 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-class Settings(BaseSettings):
-    database_url: str
-    env: str = "production"
-
-    model_config = SettingsConfigDict(
-        env_file = ".env",
-        env_file_encoding= "utf-8",
-        case_sensitive = False,
-        extra = "ignore"
-    )
-
-settings = Settings()
+from ask_sg.core.config import settings
 
 engine = create_engine(settings.database_url,
                        echo=settings.env == "development",
