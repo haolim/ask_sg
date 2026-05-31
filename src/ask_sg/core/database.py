741fe98d5f9c6ee486a08fbeea2e5e237e50e6c6
Hao <haolim84@gmail.com>
2026-05-31 12:13:44 +0800

    refactor to pydantic v2 typesafe config

--- src/ask_sg/core/database.py
+++ src/ask_sg/core/database.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
     env: str = "production"
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        env_file_encoding= "utf-8",
+        case_sensitive = False,
+        extra = "ignore"
+    )
 
 settings = Settings()
 
