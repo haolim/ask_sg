a6a62fd1a30701e7a9f3c2cbd1d099ba5305cf1a
Hao <haolim84@gmail.com>
2026-08-06 17:39:57 +0800

    feat(frontend): skeleton of the chat UI with hardcoded message

---
+++ frontend/app/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { useState } from "react";
+
+type Message = {
+  text: string;
+  role: string;
+} 
+
+export default function Home() {
+  const [inputText, setInputText] = useState("");
+  const [userMessages, setUserMessages] = useState<Message[]>([
+    {text: "How can I help you today?", role: "system"},
+    {text: "Median price in Bishan?", role: "user"},
+    {text: "I'm sorry, there is no information found for Bishan.", role: "system"}
+  ]);
+  function handleSend(newText: string) {
+    setUserMessages([...userMessages, {text: newText, role: "user"}]);
+    setInputText("")
+  }
+  return(
+    <main>
+      <h1>ask_sg</h1>
+      <div>
+      <ChatInput
+        inputText={inputText}
+        onInputTextChange={setInputText} 
+        onSend={handleSend}/>
+      </div>
+
+      <div>
+        {userMessages.map( (m, i) => <MessageBubble key={i} text={m.text} role={m.role} />)}
+      </div>
+    </main>
+  );
+}
+
+function MessageBubble( {text, role}: {text: string; role: string}) {
+  return (
+    <div>
+      {role === "user" ? "You: " : "Bot: "}
+      {text}
+    </div>
+  );
+}
+
+function ChatInput({ inputText, 
+  onInputTextChange,
+  onSend }: {inputText: string; onInputTextChange: (value: string) => void; onSend: (value: string) => void }) {
+  return (
+    <div>
+      <input type="text"
+      value={inputText}
+      placeholder="Ask about HDB resale..."
+      onChange={ (e) => onInputTextChange(e.target.value)}/>
+      <button onClick={() => onSend(inputText)}>
+        Send!
+      </button>
+    </div>
+  );
+}
+
